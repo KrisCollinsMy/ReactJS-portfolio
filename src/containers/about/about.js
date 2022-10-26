9963218e9f35b3572d1abbac8019b0e72a26cb2a
kriscollinsmy <kriscollinsmy@gmail.com>
2022-10-25 21:57:30 -0500

    [Add] ReactJS portfolio scene

--- src/containers/about/about.js
+++ src/containers/about/about.js
@@ -8,27 +8,6 @@ const About = props => {
       <div className="about-image">
         <img src={"/images/profile2.png"} alt="me" />
         <div className="about-links">
-          <a
-            href="https://www.linkedin.com/in/miguel-jimenez-benajes-2a2a9752"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={"/images/linkedin.svg"} alt="linked-in-link" />
-          </a>
-          <a
-            href="https://github.com/mijim"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={"/images/github.svg"} alt="github-in-link" />
-          </a>
-          <a
-            href="https://dev.to/mijim"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={"/images/dev-to.svg"} alt="dev-to-in-link" />
-          </a>
         </div>
       </div>
       <div className="about-text">
